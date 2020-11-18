ab45320e16a2c489d7b01fbd21678ea6c7419a6f
phil <phil@penguin>
2020-11-18 23:36:08 +0000

    got rid of cascasde DDLs, bit more on part model

--- AlsoFits.core/Model/Part.cs
+++ AlsoFits.core/Model/Part.cs
@@ -4,8 +4,36 @@ namespace AlsoFits.core.Model
         public int PartId{get;set;}
         public string FullName{get;set;}
 
-
         
+        
+    }
+
+    public class PartInstance
+    {
+        public int PartInstanceId {get;set;}
+
+        public int PartId { get; set; }
+
+        public string PartSupplier { get; set; }
+
+        public string PartNumber1 { get; set; }
+        public string PartNumber2 { get; set; }
+        public string PartNumber3 { get; set; }
+
+
+    }
+
+    public class PartVendor
+    {
+        public int PartVendorId { get; set; }
+        public int PartInstanceId { get; set; }
+
+        public string VendorName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CurrencyCode { get; set; }
+        public string VendorPdpUrl { get; set; }
     }
 
 
