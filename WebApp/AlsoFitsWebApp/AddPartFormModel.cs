ff750b8fbc959a348f25546a605b86ebfa20a3a2
phil <phil@penguin>
2020-12-03 22:39:23 +0000

    Added validation to the addpart form

--- WebApp/AlsoFitsWebApp/AddPartFormModel.cs
+++ WebApp/AlsoFitsWebApp/AddPartFormModel.cs
@@ -7,16 +7,22 @@ namespace AlsoFitsWebApp
         [Required]
         public string Description { get; set; } 
         
-        [UrlAttribute]
+        [Required(ErrorMessage="Please enter a valid URL where you can buy the part")]
         public string PdpUrl { get; set; }
 
         [Required]
         public string PartCategory { get; set; }
+       
+        
         [Required]
+        [Range(1, 99999999, ErrorMessage = "Please choose a valid car model.")]
         public int SourceModelId { get; set; }
+
         [Required]
+        [Range(1, 99999999, ErrorMessage = "Please choose a valid car model to which your part alsofits.")]
         public int DestinationModelId { get; set; }
 
+        [Required]
         public string PartNumber1 { get; set; }
 
         public string PartNumber2 { get; set; }
@@ -24,6 +30,7 @@ namespace AlsoFitsWebApp
         public string PartNumber3    { get; set; }
 
         [Required]
+        [Range(0.01, 25000, ErrorMessage = "Enter a price up to 25,000.")]
         public decimal Price { get; set; }
 
         public string CurrencySymbol {get;set;}
